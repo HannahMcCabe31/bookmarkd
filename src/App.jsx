242b8a19b8c8105327ae5e27b505e2d8aa11bb86
Adam <adam.alrasi@outlook.com>
2023-12-01 11:36:03 +0000

    change to the title

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>bookmarkd</h1>
+      <h1>Team bookmarkd</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
