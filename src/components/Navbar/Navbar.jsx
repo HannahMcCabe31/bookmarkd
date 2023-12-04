ca8ff64869c248ca4ab6b1d8dcf38ba827cec67c
James Robertson <jaarobertson92@gmail.com>
2023-12-04 13:45:50 +0000

    dashboard route specified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar()  {
     return (
         <nav className="bg-navbar p-4 fixed bottom-0 left-0 w-full flex justify-between items-center">
                 <Link to="/profile" className="text-button-beige">Profile</Link>
-                <Link to="/" className="text-button-beige">Dashboard</Link>
+                <Link to="/dashboard" className="text-button-beige">Dashboard</Link>
                 <Link to="/search" className="text-button-beige">Search</Link>
         </nav>
     );
