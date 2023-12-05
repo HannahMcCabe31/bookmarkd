eedf4e3459172bf4ab5ed79cdc3f0737e204764f
James Robertson <jaarobertson92@gmail.com>
2023-12-05 08:59:39 +0000

    created white tailwind colour

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ function Dashboard() {
     return (
         <>
         <div>
-            <h1 className="text-white-500">Dashboard</h1>
+            <h1 className="text-white">Dashboard</h1>
             <Link to="/recommendations">Recommendations</Link>
         </div>
         </>
