a9f36fedcc4af78da5af389058ab9ad2fd26d25a
Ameenah Jalil <ameenah_234@icloud.com>
2023-12-05 11:19:25 +0000

    Added the resize feature message and logout function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import Dashboard from "../Dashboard/Dashboard";
+import Dashboard from "../Dashboard/Dashboard.jsx"
 import Profile from "../Profile/Profile";
 import Search from "../Search/Search";
 import LoginPage from "../../non_tailwind_components/LoginPage/LoginPage.jsx";
+
 
 function App() {
     const [token, setToken] = useState(false);
@@ -21,20 +22,44 @@ function App() {
         }
     }, []);
 
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<LoginPage setToken={setToken} />} />
-                {token ? (
-                    <Route path="/" element={<Navbar />}>
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/search" element={<Search />} />
-                    </Route>
-                ) : null}
-            </Routes>
-        </Router>
-    );
-}
+//     return (
+//         <Router>
+//             <Routes>
+//                 <Route path="/" element={<LoginPage setToken={setToken} />} />
+//                 {token ? (
+//                   <>
+                 
+//                     <Route path="/" element={<Navbar />}>
+//                     <Route path="/dashboard" element={<Dashboard token={token} />} />
+//                         <Route path="/profile" element={<Profile />} />
+//                         <Route path="/search" element={<Search />} />
+//                     </Route>
+//                     </>
+//                 ) : null}
+                
+//             </Routes>
+//         </Router>
+//     );
+// }
+return (
+  <Router>
+      {token && <Navbar />} {/* Render Navbar if token is present */}
+      <Routes>
+          <Route path="/" element={<LoginPage setToken={setToken} />} />
+          
+          {/* Redirect to login if no token */}
+          {!token && <Route path="/dashboard" element={<Navigate to="/" />} />}
+          {!token && <Route path="/profile" element={<Navigate to="/" />} />}
+          {!token && <Route path="/search" element={<Navigate to="/" />} />}
+          {/* {!token && <Route path="/recommendations" element={<Navigate to="/" />} />} */}
 
-export default App;
+          {/* Protected routes */}
+          {token && <Route path="/dashboard" element={<Dashboard token={token} />} />}
+          {token && <Route path="/profile" element={<Profile />} />}
+          {token && <Route path="/search" element={<Search />} />}
+          {/* {token && <Route path="/recommendations" element={<Recommendations />} />} */}
+      </Routes>
+  </Router>
+);
+
+}export default App;
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,51 @@
-function Dashboard() {
-    return (
-        <div>
-            <h1>Dashboard</h1>
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+function Dashboard(props) {
+  let navigate = useNavigate();
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    function handleResize() {
+      const screenSize = window.innerWidth;
+      if (screenSize < 450) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    }
+    window.addEventListener("resize", handleResize);
+  });
+
+  function handleLogout() {
+    sessionStorage.removeItem("token");
+    navigate("/");
+  }
+  return (
+    <div>
+      <h1 className="text-white">Dashboard</h1>
+      {isMobile ? (
+        <h1>Mobile</h1>
+      ) : (
+        <div className="h-screen flex flex-col items-center justify-center text-3xl text-white ">
+          <div className="max-w-md">
+            <h1 className="mb-3">
+              Hello, {props.token.user.user_metadata.username}
+            </h1>
+            <p className="mb-5">
+              For an optimal mobile experience, please resize your browser
+              window to a mobile size screen. This will ensure you have access
+              to all of the website's features.
+            </p>
+          </div>
+          <button
+            className="border p-2 hover:text-star-blue"
+            onClick={handleLogout}
+          >
+            LOG OUT
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Dashboard;
