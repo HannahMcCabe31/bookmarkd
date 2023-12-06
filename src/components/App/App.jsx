67a9a441f651e2be82cf8f3e0d90bea5407da217
dredwerkz <60428436+dredwerkz@users.noreply.github.com>
2023-12-06 09:47:17 +0000

    Updating routes and changing images

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -20,6 +20,7 @@ import AIPowered from "../AIPowered/AIPowered.jsx";
 import ContactUs from "../ContactUs/ContactUs.jsx";
 import BookPage from "../BookPage/BookPage.jsx";
 
+
 function App() {
   const [token, setToken] = useState(false);
 
@@ -43,27 +44,32 @@ function App() {
             <Route path="/" element={<LoginPage setToken={setToken} />} />
 
             {/* Redirect to login if no token */}
-            {!token && (
-              <Route path="/dashboard" element={<Navigate to="/" />} />
-            )}
-            {!token && <Route path="/profile" element={<Navigate to="/" />} />}
-            {!token && <Route path="/search" element={<Navigate to="/" />} />}
-            {!token && (
-              <Route path="/recommendations" element={<Navigate to="/" />} />
-            )}
+            {!token && <Route path="/dashboard" element={<Navigate to="/" />} />}
+        {!token && <Route path="/profile" element={<Navigate to="/" />} />}
+        {!token && <Route path="/search" element={<Navigate to="/" />} />}
+        {!token && (
+          <Route path="/recommendations" element={<Navigate to="/" />} />
+        )}
+        {!token && <Route path="/settings" element={<Navigate to="/" />} />}
 
             {/* Protected routes */}
             {token && (
-              <Route path="/dashboard" element={<Dashboard token={token} />} />
-            )}
-            {token && <Route path="/profile" element={<Profile />} />}
-            {token && <Route path="/search" element={<Search />} />}
-            {token && (
-              <Route
-                path="/recommendations"
-                element={<Recommendations />}
-              />
-            )}
+          <Route path="/dashboard" element={<Dashboard token={token} />} />
+        )}
+        {token && <Route path="/profile" element={<Profile />} />}
+        {token && <Route path="/search" element={<Search />} />}
+        {token && (
+          <Route path="/recommendations" element={<Recommendations />} />
+        )}
+        {token && <Route path="/friends" element={<Friends />} />}
+        {token && <Route path="/settings" element={<Settings />} />}
+        {token && <Route path="/privacy-policy" element={<PrivacyPolicy />} />}
+        {token && (
+          <Route path="/terms-and-conditions" element={<TermsConditions />} />
+        )}
+        {token && <Route path="/contact-us" element={<ContactUs />} />}
+        {token && <Route path="/ai-powered" element={<AIPowered />} />}
+        {token && <Route path="/book-page" element={<BookPage />} />}
           </Routes>
         </div>
       </Router>
