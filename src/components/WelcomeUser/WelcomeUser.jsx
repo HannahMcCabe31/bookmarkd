fe76b6bb2e5f6f0bd1549a6523e71361f176f26b
Ameenah Jalil <ameenah_234@icloud.com>
2023-12-06 11:44:46 +0000

    Changed WelcomeUser.jsx into material ui

--- src/components/WelcomeUser/WelcomeUser.jsx
+++ src/components/WelcomeUser/WelcomeUser.jsx
@@ -1,14 +1,19 @@
-/* */
+import {Box} from "@mui/material"
+import Typography from "@mui/material/Typography"
+import { ThemeProvider } from "@mui/material/styles"
+import {bookmarkd} from "../../definitions/bookmarkdTheme"
 
 function WelcomeUser(props) {
 
     return (
-        <div className="mt-10 ml-4">
-        <div className="w-24 h-24 mb-4 mt-3 bg-white border rounded-full"></div>
-        <h1 className="ml-5 mb-3 text-3xl">
+      <ThemeProvider theme={bookmarkd}>
+        <Box component="section" className="mt-10 ml-4">
+        <Box className="w-24 h-24 mb-4 mt-3 bg-white border rounded-full"/>
+        <Typography variant="h2">
           Hello, {props.token.user.user_metadata.username}!
-        </h1>
-      </div>
+        </Typography>
+      </Box>
+      </ThemeProvider>
     )
 }
 
