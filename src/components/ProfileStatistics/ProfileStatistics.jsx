25110b385b278c2f6520200ae970a7a70e5b5aa8
Ameenah Jalil <ameenah_234@icloud.com>
2023-12-07 16:35:09 +0000

    Added some styling to the statistics and the flame icon

--- src/components/ProfileStatistics/ProfileStatistics.jsx
+++ src/components/ProfileStatistics/ProfileStatistics.jsx
@@ -1,3 +1,5 @@
+import flame from "../../assets/Flame.svg";
+
 import React, { useState, useEffect } from "react";
 import { Box, TextField } from "@mui/material";
 import Typography from "@mui/material/Typography";
@@ -8,16 +10,27 @@ function ProfileStatistics() {
 
     return(
         <ThemeProvider theme={bookmarkd}>
+            <div className="p-3">
         <Typography variant="h5" className="text-white">
                             Your Statistics</Typography>
-        <Box className="bg-element-blue">
+        <Box className="bg-element-blue rounded-3xl grid grid-cols-3 my-3 p-3">
+
+<div>     
 <Typography variant="h4">Total Books Read: 24</Typography>
 {/* needs icons */}
+</div> 
+<div>
 <Typography variant="h4">Total Pages Read: 20,000</Typography>
 {/* needs isons */}
+</div>
+<div>
+<img src={flame} alt="Flame" />
 <Typography variant="h4">Reading Streak: 24</Typography>
 {/* needs icons */}
+
+</div>
 </Box>
+</div>
 </ThemeProvider>
 
 
