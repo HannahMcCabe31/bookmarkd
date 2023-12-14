aaf89a55e1a81de3827260fe2b3627df01558fc4
dredwerkz <60428436+dredwerkz@users.noreply.github.com>
2023-12-14 15:13:10 +0000

    Added toggle for navbar on side

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,12 +6,14 @@ import { bookmarkd } from "../../definitions/bookmarkdTheme.jsx";
 import { ThemeProvider } from "@mui/material/styles";
 import { supabase } from "../Supabase/client.js";
 import AppRoutes from "../AppRoutes/AppRoutes.jsx";
+import DesktopNavbar from "../DesktopNavbar/DesktopNavbar.jsx"
 
 export const TokenContext = createContext();
 export const SetTokenContext = createContext();
 
 function App() {
     const [token, setToken] = useState(false);
+    const [isMobile, setIsMobile] = useState(true);
 
     function handleResize() {
         const screenSize = window.innerWidth;
@@ -21,6 +23,17 @@ function App() {
           setIsMobile(false);
         }
       }
+
+      useEffect(() => {
+        window.addEventListener("resize", handleResize);
+    
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+    
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        };
+      }, []);
 
     if (token) {
         sessionStorage.setItem("token", JSON.stringify(token));
@@ -39,7 +52,8 @@ function App() {
             <SetTokenContext.Provider value={setToken}>
                 <ThemeProvider theme={bookmarkd}>
                     <Router>
-                        {token && <Navbar />}
+                        {token && isMobile && <Navbar />}
+                        {token && !isMobile && <DesktopNavbar />}
                         <div className="pb-16">
                             <AppRoutes token={token} />
                         </div>
