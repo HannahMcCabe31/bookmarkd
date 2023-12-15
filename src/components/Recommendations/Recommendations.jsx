913feb86569f59e044b60823f96797272c8d73aa
Ameenah Jalil <ameenah_234@icloud.com>
2023-12-15 11:03:36 +0000

    Added in useeffect in recommendations

--- src/components/Recommendations/Recommendations.jsx
+++ src/components/Recommendations/Recommendations.jsx
@@ -28,6 +28,24 @@ function Recommendations() {
   function saveRecommendations() {
     setRecommendations([]);
   }
+  useEffect(()=> {
+  async function fetchUserBookData(user_id, book_id) {
+    const userBookData = await fetch(
+      `https://bookmarkd-server.onrender.com/api/user_book_data?book_id=${book_id}&user_id=${user_id}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    if (userBookData.ok) {
+      const data = await userBookData.json();
+
+              return data.payload;
+          }
+      }})
 
   return (
     <div>
