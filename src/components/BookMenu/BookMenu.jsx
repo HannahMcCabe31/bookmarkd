826e748168c152750548ea9514e2871ea4201e0b
dredwerkz <60428436+dredwerkz@users.noreply.github.com>
2023-12-19 21:51:36 +0000

    Swapped state setter to top of function to avoid desync issues with fetch requests

--- src/components/BookMenu/BookMenu.jsx
+++ src/components/BookMenu/BookMenu.jsx
@@ -13,12 +13,7 @@ function BookMenu({
     const [liked, setLiked] = useState(false);
     const [completed, setCompleted] = useState(false);
 
-    function bookshelfEditMode() {
-        setEditingBookshelf((mode) => !mode);
-    }
-
     useEffect(() => {
-        // Grab user favs from fav bookshelf to check if this book is in there
         async function getBookshelves() {
             const responseRequest = await fetch(
                 `https://bookmarkd-server.onrender.com/api/bookshelves?user_id=${token.user.id}`,
@@ -49,105 +44,70 @@ function BookMenu({
                     (id) => String(id) === String(book_id)
                 );
 
-                return [bookFavourited, bookCompleted];
-            } else if (!responseRequest.ok) {
+                setLiked(bookFavourited);
+                setCompleted(bookCompleted);
+            } else {
                 console.error(`Status: ${responseRequest.status}`);
                 console.error(`Text: ${await responseRequest.text()}`);
                 console.error("Data not available");
-                return;
             }
         }
 
-        getBookshelves()
-            .then((payload) => {
-               // if (payload[0] === true && liked === false) {
-                setLiked(payload[0]);
-              //  
-                setCompleted(payload[1]);
-            })
-            .catch((error) => {
-                console.error(`Error fetching: ${error}`);
-            });
-    }, [liked, completed]);
+        getBookshelves();
+    }, [token.user.id, book_id]);
 
     async function likeButton() {
-        async function updateFavs(action) {
-            const fetchMethod = (() => {
-                switch (!action) {
-                    case true:
-                        return "PATCH";
-                    case false:
-                        return "DELETE";
-                }
-            })();
-            console.log(`book_id is:`);
-            console.log(typeof book_id);
-            const responseRequest = await fetch(
-                `https://bookmarkd-server.onrender.com/api/fav?user_id=${token.user.id}&book_id=${book_id}`,
-                {
-                    method: `${fetchMethod}`,
-                    headers: {
-                        Accept: "application/json",
-                    },
-                }
-            );
+        setLiked((prevLiked) => !prevLiked);
 
-            if (responseRequest.ok) {
-                const responseData = await responseRequest.json();
-
-                return responseData;
-            } else if (!responseRequest.ok) {
-                console.error(`Status: ${responseRequest.status}`);
-                console.error(`Text: ${await responseRequest.text()}`);
-                console.error("Data not available");
-                return;
+        const method = liked ? "DELETE" : "PATCH";
+        const responseRequest = await fetch(
+            `https://bookmarkd-server.onrender.com/api/fav?user_id=${token.user.id}&book_id=${book_id}`,
+            {
+                method: method,
+                headers: {
+                    Accept: "application/json",
+                },
             }
+        );
+
+        if (!responseRequest.ok) {
+            setLiked((prevLiked) => !prevLiked); // Revert the liked state if the request fails
+            console.error(`Status: ${responseRequest.status}`);
+            console.error(`Text: ${await responseRequest.text()}`);
+            console.error("Data not available");
         }
-        setLiked((liked) => !liked);
-        updateFavs(liked);
     }
 
     async function completedButton() {
-        async function updateCompleted(action) {
-            const fetchMethod = (() => {
-                switch (!action) {
-                    case true:
-                        return "PATCH";
-                    case false:
-                        return "DELETE";
-                }
-            })();
-            const responseRequest = await fetch(
-                `https://bookmarkd-server.onrender.com/api/complete?user_id=${token.user.id}&book_id=${book_id}`,
-                {
-                    method: `${fetchMethod}`,
-                    headers: {
-                        Accept: "application/json",
-                    },
-                }
-            );
+        setCompleted((prevCompleted) => !prevCompleted);
 
-            if (responseRequest.ok) {
-                const responseData = await responseRequest.json();
-
-                return responseData;
-            } else if (!responseRequest.ok) {
-                console.error(`Status: ${responseRequest.status}`);
-                console.error(`Text: ${await responseRequest.text()}`);
-                console.error("Data not available");
-                return;
+        const method = completed ? "DELETE" : "PATCH";
+        const responseRequest = await fetch(
+            `https://bookmarkd-server.onrender.com/api/complete?user_id=${token.user.id}&book_id=${book_id}`,
+            {
+                method: method,
+                headers: {
+                    Accept: "application/json",
+                },
             }
+        );
+
+        if (!responseRequest.ok) {
+            setCompleted((prevCompleted) => !prevCompleted); // Revert the completed state if the request fails
+            console.error(`Status: ${responseRequest.status}`);
+            console.error(`Text: ${await responseRequest.text()}`);
+            console.error("Data not available");
         }
-        setCompleted((completed) => !completed);
-        updateCompleted(completed);
+    }
+
+    function bookshelfEditMode() {
+        setEditingBookshelf((mode) => !mode);
     }
 
     const buttonStyling = "w-[10vw] md:w-auto md:h-[15%]";
 
     return (
-        <Box
-            className={`flex justify-around flex-row mt-[5vw] md:mt-[10vw]  ${leftElementClass}`}
-        >
+        <Box className={`flex justify-around flex-row mt-[5vw] md:mt-[10vw] ${leftElementClass}`}>
             <img
                 className={`${buttonStyling}`}
                 src={liked ? "/img/liked_icon.png" : "/img/not_liked_icon.png"}
@@ -160,11 +120,7 @@ function BookMenu({
             />
             <img
                 className={`${buttonStyling}`}
-                src={
-                    completed
-                        ? "/img/completed_icon.png"
-                        : "/img/not_completed_icon.png"
-                }
+                src={completed ? "/img/completed_icon.png" : "/img/not_completed_icon.png"}
                 onClick={completedButton}
             />
         </Box>
