7a54cc0100767aa4b892d1a1a2b3d47a68684f29
HannahMcCabe31 <hmccabe.hm@gmail.com>
2023-12-20 14:42:36 +0000

    finished making search page responsive

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,5 @@
 import { Box, TextField } from "@mui/material";
+import { SearchPageBar } from "../../definitions/CustomComponents";
 import Typography from "@mui/material/Typography";
 import backArrow from "../../assets/BackArrow.svg";
 import { Link } from "react-router-dom";
@@ -71,46 +72,50 @@ function Search() {
     }, [searchQuery]);
 
     return (
-        <div className=" md:max-w-[85%] md:pl-[20%]">
-            <div className="bg-search h-screen flex flex-col text-white p-[3vw]">
-                <Box className="flex text-center items-center md:hidden">
-                    <Link to="/dashboard">
-                        <img
-                            src={backArrow}
-                            alt="backArrow"
-                            className="w-[8vw] h-[8vw]"
-                        />
-                    </Link>
+      <div className=" md:max-w-[85%] md:pl-[20%]">
+        <div className="bg-search h-screen flex flex-col md:items-center sm:items-center text-white p-[3vw]">
+          <Box className="flex text-center items-center md:hidden lg:hidden sm:hidden">
+            <Link to="/dashboard">
+              <img
+                src={backArrow}
+                alt="backArrow"
+                className="w-[8vw] h-[8vw]"
+              />
+            </Link>
+          </Box>
+          <Box className="flex flex-col items-center">
+            <Typography
+              variant="h1"
+              className="mt-[5vw] md:text-6xl"
+            >
+              Search
+            </Typography>
 
-                    <Typography variant="h1" className="mt-[5vw] ml-[25%]">
-                        Search
-                    </Typography>
-                </Box>
+            <form className="text-center" onSubmit={handleSearch}>
+              <SearchPageBar
+                onChange={handleUpdateQuery}
+                className=" bg-element-blue border-element-blue rounded-[5vw] m-0 mt-[10vw] p-0 w-[60vh] md:mt-[5vh] md:w-[80vh] md:p-[0.2rem] md:mb-[5vh]"
+                placeholder="Search for books, users, authors..."
+                inputProps={{
+                  "aria-label": "search",
+                  style: { color: "white" },
+                }}
+                color="starBlue"
+                variant="outlined"
+              />
+            </form>
 
-                <form className="text-center" onSubmit={handleSearch}>
-                    <TextField
-                        onChange={handleUpdateQuery}
-                        className="border bg-element-blue rounded-[15vw] m-0 mt-[10vw] p-0 w-4/5 md:mt-[5vh] md:w-[80vh] md:rounded-[5vw] md:p-[0.2rem] md:mb-[5vh]"
-                        placeholder="Search for books, users, authors..."
-                        inputProps={{
-                            "aria-label": "search",
-                            style: { color: "white" },
-                        }}
-                        color="starBlue"
-                        variant="outlined"
-                    />
-                </form>
-
-                <Box>
-                    <div className="md:ml-[12vh]">
-                        <SearchContainer
-                            searchResults={searchResults}
-                            searchQuery={searchQuery}
-                        />
-                    </div>
-                </Box>
-            </div>
+            <Box>
+              <div className="md:ml-[12vh]">
+                <SearchContainer
+                  searchResults={searchResults}
+                  searchQuery={searchQuery}
+                />
+              </div>
+            </Box>
+          </Box>
         </div>
+      </div>
     );
 }
 
